5c913fccf668d7c6f6b3d158e00709baa51c3ec2
tarun.kumar <tarun.kumar@metricstream.com>
2023-07-10 23:49:00 +0530

    first commit

--- router.js
+++ router.js
@@ -46,12 +46,12 @@ Router.get("/api/user",(req, res)=>{
     
         // Initialize an array to store the retrieved documents
         
-
+        const dataResponse = [];
 collection.all().then(
   cursor => cursor.all()
 ).then(
   documents => documents.forEach(document => 
-    { const dataResponse = [];
+    { 
     let obj = {versionName:document.versionNum,email:document.createdBy,description:document.description,status:document.status};
     console.log(obj);
     dataResponse.push(obj);
