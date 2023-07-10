7cc29fd64fb3933ef54aeb36276e0b9f352b61ef
tarun.kumar <tarun.kumar@metricstream.com>
2023-07-10 20:19:49 +0530

    first commit

--- router.js
+++ router.js
@@ -20,7 +20,7 @@ const collectionName = 'msiVersionCollection';
 
 // Create a new ArangoDB connection
 const db = new arangojs.Database({
-  url: 'tcp://172.31.34.233:8529',
+  url: 'http://172.31.34.233:8529',
   databaseName: 'msidb',
   auth: { username: 'root', password: 'welcome*123' }
 });
